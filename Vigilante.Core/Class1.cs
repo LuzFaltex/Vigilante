971499346a27edcf32b6c96b5ad0837ea2eee2ee
foxtrek64 <foxtrek64@gmail.com>
2019-04-24 05:18:07 -0500

    Initial Project Structure

---
+++ Vigilante.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Vigilante.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ Vigilante.Database/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Vigilante.Database
+{
+    public class Class1
+    {
+    }
+}
